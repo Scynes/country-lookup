7ce9a59a5198c30e2fd3218ba34e00dfa5de3682
Scynes <dustin.greyfield@gmail.com>
2023-02-20 20:43:34 -0500

    wip-landing: inputs for search and region, refactored grid

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,22 +1,21 @@
-<div id="lookup-container" class="height-full margin-left-med margin-right-med">
-    <div id="results">
-        <div>
-            <input type="search" name="" id="">
-        </div>
-        <div class="card">
-            test
-        </div>
-        <div class="card">
-            test
-        </div>
-        <div class="card">
-            test
-        </div>
-        <div class="card">
-            test
-        </div>
-        <div class="card">
-            test
-        </div>
+<div id="lookup-container">
+    <div class="flex space-between wrap gap-med">
+        <input type="search" name="" id="">
+        <input type="text" placeholder="test">
+    </div>
+    <div class="card">
+        test
+    </div>
+    <div class="card">
+        test
+    </div>
+    <div class="card">
+        test
+    </div>
+    <div class="card">
+        test
+    </div>
+    <div class="card">
+        test
     </div>
 </div>
