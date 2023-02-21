567f3d92e347765ddeeb68d8c8bd2dc097b91a22
Scynes <dustin.greyfield@gmail.com>
2023-02-20 21:16:53 -0500

    wip-landing: added search styling an icon

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,12 @@
 <div id="lookup-container">
     <div class="flex space-between wrap gap-med">
-        <input type="search" name="" id="">
-        <input type="text" placeholder="test">
+        <div class="flex border-radius-small overflow-hidden">
+            <div class="flex justify-center align-center height-full width-3-rem secondary-bg-col">
+                <i class="fa-solid fa-magnifying-glass primary-font-col"></i>
+            </div>
+            <input class="secondary-bg-col primary-font-col" type="search" name="" id="" placeholder="Search for a country...">
+        </div>
+        <input class="secondary-bg-col" type="text" placeholder="test">
     </div>
     <div class="card">
         test
