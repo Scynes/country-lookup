605175b99f0fe905a713820966d5b4fd76ae88e1
Scynes <dustin.greyfield@gmail.com>
2023-02-21 15:31:16 -0500

    tweak: wraped cards in anchor for linking to page data

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -13,16 +13,18 @@
         <input class="secondary-bg-col" type="text" placeholder="test">
     </div>
     {#each data.countries as country }
-        <div class="card overflow-hidden">
-            <div class="overflow-hidden">
-                <img src={country.flags.svg} alt="">
+        <a href="/test">
+            <div class="card height-full overflow-hidden">
+                <div class="overflow-hidden">
+                    <img src={country.flags.svg} alt="">
+                </div>
+                <div class="country-info primary-font-col">
+                    <h3>{country.name}</h3>
+                    <p>Population: {country.population}</p>
+                    <p>Capitol: {country.capital}</p>
+                    <p>Region: {country.region}</p>
+                </div>
             </div>
-            <div class="country-info primary-font-col">
-                <h3>{country.name}</h3>
-                <p>Population: {country.population}</p>
-                <p>Capitol: {country.capital}</p>
-                <p>Region: {country.region}</p>
-            </div>
-        </div>
+        </a>
     {/each}
 </div>
