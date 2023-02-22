6d988db59c69378597ad2efb506b27cc84856e9f
Scynes <dustin.greyfield@gmail.com>
2023-02-22 16:10:28 -0500

    complete: added refresh functionality and updating store

--- src/components/button/RefreshButton.svelte
+++ src/components/button/RefreshButton.svelte
@@ -1,5 +1,39 @@
 <script>
+    import lodash from 'lodash';
 
+    import { countries } from "@stores/countries";
+
+    const { debounce } = lodash;
+   
+    let amount = 12;
+
+    const debouncedAPICall = debounce(async () => {
+        const result = await fetch('https://restcountries.com/v2/all?fields=name,capital,region,flags,population');
+
+        if (!result.ok) return;
+
+        const data = await result.json();
+
+        const COUNTRIES = [];
+
+        while (COUNTRIES.length < amount) {
+
+            const index = Math.floor(Math.random() * data.length);
+
+            if (!COUNTRIES.includes(data[index])) 
+                COUNTRIES.push(data[index]);
+        }
+
+        countries.set(COUNTRIES);
+
+    }, 500);
+
+    const refresh = () => {
+
+        if (amount < 1) return;
+
+        debouncedAPICall()
+    }
 </script>
 
 <style>
@@ -14,8 +48,8 @@
 </style>
 
 <div id="refresh-container" class="flex gap-small width-full">
-    <button class="refresh border-radius-small secondary-bg-col primary-font-col">
+    <button class="refresh border-radius-small secondary-bg-col primary-font-col" on:click={refresh}>
         <i class="fa-solid fa-arrows-rotate"></i>
     </button>
-    <input class="secondary-bg-col primary-font-col text-center" type="number" placeholder="Amount" value="10" max="100">
+    <input class="secondary-bg-col primary-font-col text-center" type="number" placeholder="Amount" bind:value={amount} max="100">
 </div>
